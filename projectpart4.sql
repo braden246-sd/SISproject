decb570c8aceedc181a43449824de03deacffaa4
braden246-sd <braden.ouellet@edu.sait.ca>
2026-02-23 11:37:50 -0700

    Added student table

--- projectpart4.sql
+++ projectpart4.sql
@@ -15,13 +15,25 @@ DROP TABLE Credential CASCADE CONSTRAINTS;
 -- now create table in reverse order
 
 CREATE TABLE Credential (
-    CredentialID   NUMBER(10)     NOT NULL,
-    School         VARCHAR2(50)   NOT NULL,
-    Name           VARCHAR2(50)   NOT NULL,
-    Type           CHAR(2)        NOT NULL,
+    CredentialID NUMBER(10) NOT NULL,
+    School VARCHAR2(50) NOT NULL,
+    Name VARCHAR2(50) NOT NULL,
+    Type CHAR(2) NOT NULL,
     CONSTRAINT credential_pk PRIMARY KEY (CredentialID),
     CONSTRAINT credential_type_ck CHECK (Type IN ('MI','FT','CT','DP','AD','D'))
 );
 
 
 
+CREATE TABLE Student (
+   StudentID NUMBER(10) NOT NULL,
+   FirstName VARCHAR2(50) NOT NULL,
+   LastName VARCHAR(50) NOT NULL,
+   Status VARCHAR2(2) NOT NULL,
+   StatusDate Date NOT NULL,
+   Phone VARCHAR2(10) NOT NULL,
+   Email VARCHAR2(50) NOT NULL,
+   constraint student_pk PRIMARY KEY (StudentID),
+   constraint student_status_ck CHECK (Status IN ('A','AP','S','E')),
+   constraint student_email_uk UNIQUE (Email)
+);
