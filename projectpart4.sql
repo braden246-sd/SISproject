b169264f4cc0e98c62338e24783b031fc5b1e428
braden246-sd <braden.ouellet@edu.sait.ca>
2026-02-23 11:31:11 -0700

    turned off auto formatting on my VScode

--- projectpart4.sql
+++ projectpart4.sql
@@ -1,29 +1,27 @@
-   SET ECHO ON
+SET ECHO ON
 
-drop table studentcourserecord cascade constraints;
-drop table instructor_courses cascade constraints;
-drop table studentcredential cascade constraints;
-drop table credentialcourse cascade constraints;
-drop table scheduledcourse cascade constraints;
-drop table semester cascade constraints;
-drop table course cascade constraints;
-drop table instructor cascade constraints;
-drop table student cascade constraints;
-drop table credential cascade constraints;
+DROP TABLE StudentCourseRecord CASCADE CONSTRAINTS;
+DROP TABLE Instructor_Courses CASCADE CONSTRAINTS;
+DROP TABLE StudentCredential CASCADE CONSTRAINTS;
+DROP TABLE CredentialCourse CASCADE CONSTRAINTS;
+DROP TABLE ScheduledCourse CASCADE CONSTRAINTS;
+DROP TABLE Semester CASCADE CONSTRAINTS;
+DROP TABLE Course CASCADE CONSTRAINTS;
+DROP TABLE Instructor CASCADE CONSTRAINTS;
+DROP TABLE Student CASCADE CONSTRAINTS;
+DROP TABLE Credential CASCADE CONSTRAINTS;
 
--- mnow create table in reverse order
 
-create table credential (
-   credentialid number(10) not null,
-   school       varchar2(50) not null,
-   name         varchar2(50) not null,
-   type         char(2) not null,
-   constraint credential_pk primary key ( credentialid ),
-   constraint credential_type_ck
-      check ( type in ( 'MI',
-                        'FT',
-                        'CT',
-                        'DP',
-                        'AD',
-                        'D' ) )
+-- now create table in reverse order
+
+CREATE TABLE Credential (
+    CredentialID   NUMBER(10)     NOT NULL,
+    School         VARCHAR2(50)   NOT NULL,
+    Name           VARCHAR2(50)   NOT NULL,
+    Type           CHAR(2)        NOT NULL,
+    CONSTRAINT credential_pk PRIMARY KEY (CredentialID),
+    CONSTRAINT credential_type_ck CHECK (Type IN ('MI','FT','CT','DP','AD','D'))
 );
+
+
+
